9b6ebc64a5f145b047c02fc9654255cdfb4c71bd
Tringles <jjmjm2002@naver.com>
2022-03-06 18:12:23 +0900

    chore: set up project

--- src/main/java/com/tringles/tutorial/TutorialApplication.java
+++ src/main/java/com/tringles/tutorial/TutorialApplication.java
@@ -1,4 +1,4 @@
-package com.example.tutorial;
+package com.tringles.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/tringles/tutorial/TutorialApplicationTests.java
+++ src/test/java/com/tringles/tutorial/TutorialApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.tutorial;
+package com.tringles.tutorial;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
