bdfc404ae0b37463db194bbb566766397a6853c6
Tringles <jjmjm2002@naver.com>
2022-03-09 02:15:13 +0900

    feat: OAuth2 configuration

--- src/main/java/com/tringles/tutorial/config/SecurityConfig.java
+++ src/main/java/com/tringles/tutorial/config/SecurityConfig.java
@@ -1,60 +1,24 @@
 package com.tringles.tutorial.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @EnableWebSecurity()
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser(User.builder()
-                        .username("user1")
-                        .password(passwordEncoder().encode("user1"))
-                        .roles("USER"))
-                .withUser(User.builder()
-                        .username("admin")
-                        .password(passwordEncoder().encode("admin"))
-                        .roles("ADMIN"))
-        ;
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    @Autowired
+    private OAuth2SuccessHandler successHandler;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(request -> {
-                    request
-                            .antMatchers("/").permitAll()
-                            .anyRequest().authenticated()
-                    ;
-                }).
-                formLogin()
-        ;
-        http.httpBasic();
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-                .requestMatchers(
-                        PathRequest.toStaticResources().atCommonLocations()
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(successHandler)
                 )
         ;
     }
