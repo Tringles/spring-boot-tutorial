6c3d3f3f506d6027b190848b27f92ae47ae281e2
Tringles <jjmjm2002@naver.com>
2022-03-11 22:21:37 +0900

    refactor: adjust DTO path

--- src/main/java/com/tringles/tutorial/config/JWT/JwtUtil.java
+++ src/main/java/com/tringles/tutorial/config/JWT/JwtUtil.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.tringles.tutorial.domain.User;
+import com.tringles.tutorial.dto.VerifyResult;
 
 import java.time.Instant;
 
