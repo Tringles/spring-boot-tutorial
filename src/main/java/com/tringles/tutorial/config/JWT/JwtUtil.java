f9f910b6f0a4ccb5de7bfefa34ca475e4dc192ff
Tringles <jjmjm2002@naver.com>
2022-03-11 22:19:21 +0900

    feat: generate & verify JWT

---
+++ src/main/java/com/tringles/tutorial/config/JWT/JwtUtil.java
@@ -0,0 +1,39 @@
+package com.tringles.tutorial.config.JWT;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.tringles.tutorial.domain.User;
+
+import java.time.Instant;
+
+public class JwtUtil {
+
+    private static final Algorithm ALGORITHM = Algorithm.HMAC256("TRINGLES");
+    private static final long AUTH_TIME = 60 * 60;
+    private static final long REFRESH_TIME = 60 * 60 * 24 * 7;
+
+    public static String makeAuthToken(User user) {
+        return JWT.create().withSubject(user.getUsername())
+                .withClaim("exp", Instant.now().getEpochSecond() + AUTH_TIME)
+                .sign(ALGORITHM);
+    }
+
+    public static String makeRefreshToken(User user) {
+        return JWT.create().withSubject(user.getUsername())
+                .withClaim("exp", Instant.now().getEpochSecond() + REFRESH_TIME)
+                .sign(ALGORITHM);
+    }
+
+    public static VerifyResult verify(String token) {
+        try {
+            DecodedJWT verify = JWT.require(ALGORITHM).build().verify(token);
+            return VerifyResult.builder()
+                    .success(true).username(verify.getSubject()).build();
+        } catch (Exception e) {
+            DecodedJWT decode = JWT.decode(token);
+            return VerifyResult.builder()
+                    .success(false).username(decode.getSubject()).build();
+        }
+    }
+}
