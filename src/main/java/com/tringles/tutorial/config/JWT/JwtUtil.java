fea7defefaded2ab620d6f91e2e8283e4d73f2fd
Tringles <jjmjm2002@naver.com>
2022-03-14 22:42:30 +0900

    fix: decrease access token expiration time

--- src/main/java/com/tringles/tutorial/config/JWT/JwtUtil.java
+++ src/main/java/com/tringles/tutorial/config/JWT/JwtUtil.java
@@ -13,7 +13,7 @@ import java.time.Instant;
 public class JwtUtil {
 
     private static final Algorithm ALGORITHM = Algorithm.HMAC256("TRINGLES");
-    private static final long AUTH_TIME = 60 * 60;
+    private static final long AUTH_TIME = 60 * 30;
     private static final long REFRESH_TIME = 60 * 60 * 24 * 7;
 
     public static String makeAuthToken(User user) {
